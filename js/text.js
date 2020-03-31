03777f2f617aef3e21bc70df2b62a43e30924b93
1blockatatime <62171719+1blockatatime@users.noreply.github.com>
2020-03-31 17:29:38 +0100

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,5 +1,7 @@
 var allmytext = {
 rainbb:{pt: "arco-iris", fr: "arc en ciel"},
-wynb:{pt: "Hello hello, what’s your name? (Canção de Maple Leaf Learning)", fr: "Hello hello, what’s your name? (Chanson de Maple Leaf Learning)"}
+wyn1b:{pt: "Hello hello, what’s your name?", fr: "Hello hello, what’s your name?"},
+wyn1c:{pt: "(Canção de Maple Leaf Learning)", fr: "(Chanson de Maple Leaf Learning)"  
 };
+
 
